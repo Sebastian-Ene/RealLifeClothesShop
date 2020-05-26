7ade34ff228bffa90473935e4c66398dd45c790f
Sebastian-Ene <ene.sebastian.2000@gmail.com>
2020-05-26 23:18:03 +0300

    Application folder structure

---
+++ RealLifeClothesShop/src/app/classes/Products.ts
@@ -0,0 +1,8 @@
+export class Products {
+  color: string;
+  price: number;
+  constructor(color: string, price: number) {
+    this.color = color;
+    this.price = price;
+  }
+}
---
+++ RealLifeClothesShop/src/app/classes/products.spec.ts
@@ -0,0 +1,8 @@
+import { Products } from './Products';
+import { ProductsComponent } from '../products/products.component';
+
+/*describe('Products', () => {
+  it('should create an instance', () => {
+    expect(new Products(choseColor, chosePrice)).toBeTruthy();
+  });
+});*/
