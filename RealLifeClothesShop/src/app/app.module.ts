955843c548cc66dfff87a4ba309f0af9766ea12d
Sebastian-Ene <ene.sebastian.2000@gmail.com>
2020-05-27 13:31:46 +0300

    Routes, Html for navbar and footer

--- RealLifeClothesShop/src/app/app.module.ts
+++ RealLifeClothesShop/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ImplementationComponent } from './implementation/implementation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, ProductsComponent, NavBarComponent, FooterComponent, AboutComponent, ImplementationComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
