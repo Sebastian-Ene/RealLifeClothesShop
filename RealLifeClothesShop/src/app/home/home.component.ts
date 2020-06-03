9ecf8b7c05c2a8a56a1f4444a7336380352b39a2
Sebastian-Ene <ene.sebastian.2000@gmail.com>
2020-06-03 15:55:56 +0300

    Add Preset Customer, Outside queue

--- RealLifeClothesShop/src/app/home/home.component.ts
+++ RealLifeClothesShop/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../classes/customer';
+import { mockCust } from './mockCustomers';
+
 
 @Component({
   selector: 'app-home',
@@ -10,20 +12,38 @@ export class HomeComponent implements OnInit {
   displayCustomers = 'none';
   selectedCustomer: number;
   customers = [];
+  customersQueue = [];
   custId: number;
   custFName: string;
   custLName: string;
   constructor() { }
 
   ngOnInit(): void { }
-  addCustomer() {
+  addCustomer(aCustId, aCustFName, aCustLName) {
     this.displayCustomers = 'block';
-    this.customers.push(new Customer(
-      this.custId,
-      this.custFName,
-      this.custLName
-    ));
-    console.log('Added Customer');
+    if (this.customers.length < 10) {
+      this.customers.push(new Customer(
+        aCustId,
+        aCustFName,
+        aCustLName
+      ));
+      console.log('Added Customer');
+    }
+    else {
+      this.customersQueue.push(new Customer(
+        aCustId,
+        aCustFName,
+        aCustLName
+      ));
+      console.log('Added Customer to Queue');
+    }
+  }
+  addPresetCust() {
+    if (mockCust.length === 0) { alert('No more preset customers available!'); }
+    else {
+      this.addCustomer(1, mockCust[0][0], mockCust[0][1]);
+      mockCust.splice(0, 1);
+    }
   }
   selectCustomer(i: number) {
 
@@ -33,6 +53,13 @@ export class HomeComponent implements OnInit {
     this.customers.splice(i, 1);
     this.selectedCustomer = null;
     console.log('Removed Customer');
+    this.queueToShop();
+  }
+  queueToShop() {
+    if (this.customers.length < 10 && this.customersQueue.length > 0) {
+      const a = this.customersQueue.splice(0, 1);
+      this.customers.push(a[0]);
+    }
   }
 }
 
---
+++ RealLifeClothesShop/src/app/home/mockCustomers.ts
@@ -0,0 +1,12 @@
+export const mockCust = [
+  ['George', 'Smith'],
+  ['Emily', 'Jones'],
+  ['Oliver', 'Williams'],
+  ['Isabella', 'Brown'],
+  ['Jacob', 'Taylor'],
+  ['Elizabeth', 'Murphy'],
+  ['John', 'Smith'],
+  ['Megan', 'Johnson'],
+  ['Michael', 'Brown'],
+  ['Jennifer', 'Davis'],
+];
